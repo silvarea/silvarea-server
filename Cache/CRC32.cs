1f1208bcab9025f7768cf8b185474f5b87999782
TylerGC <tylergerardcole@gmail.com>
2024-04-18 19:28:17 -0400

    CRC32 utility

---
+++ Cache/CRC32.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silvarea.Cache
+{
+
+
+    public class CRC32
+    {
+        private const uint Polynomial = 0xEDB88320;
+        private static uint[] _table;
+
+        static CRC32()
+        {
+            _table = new uint[256];
+            for (uint i = 0; i < 256; i++)
+            {
+                uint crc = i;
+                for (int j = 8; j > 0; j--)
+                {
+                    if ((crc & 1) == 1)
+                    {
+                        crc = (crc >> 1) ^ Polynomial;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+                _table[i] = crc;
+            }
+        }
+
+        public static uint CalculateCrc32(byte[] data)
+        {
+            uint crc = 0xFFFFFFFF;
+            foreach (byte b in data)
+            {
+                byte index = (byte)(((crc) & 0xff) ^ b);
+                crc = (_table[index] ^ (crc >> 8));
+            }
+            return ~crc;
+        }
+    }
+}
+    
