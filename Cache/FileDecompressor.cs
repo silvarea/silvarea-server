074f59950a59da8a847b2a4bac3da818d8ee0f30
TylerGC <tylergerardcole@gmail.com>
2024-04-18 23:42:03 -0400

    FileDecompressor to handle the GZIP and BZIP2 compression

---
+++ Cache/FileDecompressor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib;
+using ICSharpCode.SharpZipLib.BZip2;
+
+namespace Silvarea.Cache
+{
+    internal class FileDecompressor
+    {
+
+        private readonly CacheFile _file;
+
+        public FileDecompressor(byte[] data)
+        {
+            MemoryStream buffer = new MemoryStream(data);
+            BinaryReader reader = new BinaryReader(buffer);
+            int compression = reader.ReadByte();
+            int uncompressedSize = reader.ReadInt32();
+            int cacheSize = compression != 0 ? reader.ReadInt32() : uncompressedSize;
+            _file = new CacheFile(buffer, compression, uncompressedSize, cacheSize);
+        }
+
+        public byte[] decompress()
+        {
+            byte[] newData = new byte[_file.CacheSize];
+            switch (_file._Compression)
+            {
+                case CacheFile.Compression.NONE:
+                    Array.Copy(_file.toByteArray(), 5, newData, 0, _file.CacheSize);
+                    break;
+                case CacheFile.Compression.BZIP:
+                    BZip2InputStream bz2decompress = new BZip2InputStream(_file.Data);
+                    bz2decompress.Read(newData, 9, newData.Length);
+                    break;
+                case CacheFile.Compression.GZIP:
+                    GZipStream gzdecompress = new GZipStream(_file.Data, CompressionMode.Decompress);
+                    gzdecompress.Read(newData, 9, newData.Length);
+                    break;
+            }
+            return newData;
+        }
+
+    }
+}
